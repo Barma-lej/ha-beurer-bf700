1468c86591f015a7ce9133cd021cd6eeaaeaa853
Barma-lej <34010886+Barma-lej@users.noreply.github.com>
2026-02-11 14:07:45 +0100

    Change CONF_MAC to custom mac_address string
    Updated CONF_MAC to use a custom string instead of importing from homeassistant.

--- custom_components/beurer_bf700/const.py
+++ custom_components/beurer_bf700/const.py
@@ -1,17 +1,16 @@
 """Константы для Beurer BF 700."""
-from homeassistant.const import CONF_MAC_ADDRESS
 
 DOMAIN = "beurer_bf700"
 
 # Конфигурация
-CONF_MAC = CONF_MAC_ADDRESS
+CONF_MAC = "mac_address"
 
-# Протокол BF 700
+# Протокол BF 700 (из openScale)
 CMD_START = 0xF7
 CMD_INIT = 0xF6
 CMD_SYNC = 0xF9
 
-# Bluetooth UUID (из openScale)
+# Bluetooth UUID
 SERVICE_UUID = "0000fff0-0000-1000-8000-00805f9b34fb"
 WRITE_CHAR_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"
 NOTIFY_CHAR_UUID = "0000fff4-0000-1000-8000-00805f9b34fb"
