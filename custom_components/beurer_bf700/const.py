9a40f029cf0e2081a1e707cd8c9c26e3ff1282f5
Barma-lej <34010886+Barma-lej@users.noreply.github.com>
2026-02-11 13:53:51 +0100

    Update constants and comments for Beurer BF 700

--- custom_components/beurer_bf700/const.py
+++ custom_components/beurer_bf700/const.py
@@ -1,12 +1,18 @@
+"""Константы для Beurer BF 700."""
 DOMAIN = "beurer_bf700"
+
+# Конфигурация
 CONF_MAC = "mac_address"
 
-# Команды протокола BF 700
+# Протокол BF 700
 CMD_START = 0xF7
 CMD_INIT = 0xF6
 CMD_SYNC = 0xF9
 
-# UUID сервисов (из openScale)
+# Bluetooth UUID (из openScale)
 SERVICE_UUID = "0000fff0-0000-1000-8000-00805f9b34fb"
 WRITE_CHAR_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"
 NOTIFY_CHAR_UUID = "0000fff4-0000-1000-8000-00805f9b34fb"
+
+# Имя устройства для автообнаружения
+DEVICE_NAME = "BEURER BF700"
